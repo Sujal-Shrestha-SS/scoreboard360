8d9c3cc056b296a490ce9e3870522beaeff7b726
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2025-07-26 14:04:39 +0545

    update result

---
+++ admin/update_result.php
@@ -0,0 +1,47 @@
+<?php
+
+
+$conn = mysqli_connect("localhost", "root", "", "scoreboard360");
+
+$fixture_id = $_POST['fixture_id'];
+$home_score = $_POST['home_score'];
+$away_score = $_POST['away_score'];
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// 1️⃣ Update new score
+$sql = "UPDATE results SET home_score = $home_score , away_score = $away_score WHERE fixture_id = $fixture_id";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+    echo "Update Successful";
+    
+
+    $statusSql = "
+    UPDATE results r
+    JOIN add_fixture af ON r.fixture_id = af.fixture_id
+    SET r.status = 
+      CASE
+        WHEN r.home_score > r.away_score THEN af.home_team
+        WHEN r.home_score < r.away_score THEN af.away_team
+        ELSE 'Draw'
+      END
+    WHERE r.fixture_id = '$fixture_id'
+  ";
+
+  if (mysqli_query($conn, $statusSql)) {
+    echo "Match status updated successfully!";
+  } else {
+    echo "Error updating status: " . mysqli_error($conn);
+  }
+}
+
+else{
+    echo "Update failed";
+}
+
+
+mysqli_close($conn);
+?>
